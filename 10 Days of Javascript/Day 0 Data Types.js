6dff9db05013da727b050b2bf82968a20e876c87
BadrChoujai <57566300+Badr504@users.noreply.github.com>
2020-06-08 14:02:11 +0100

    Modified Comment in this file

--- 10 Days of Javascript/Day 0 Data Types.js
+++ 10 Days of Javascript/Day 0 Data Types.js
@@ -5,16 +5,13 @@ Problem Link: https://www.hackerrank.com/challenges/js10-data-types/problem
 
 */
 
-
-// Solution: 
+//This Solution is Nicely Complicated:
 function performOperation(secondInteger, secondDecimal, secondString) {
+  const firstInteger = 4;
+  const firstDecimal = 4.0;
+  const firstString = "HackerRank ";
 
-    const firstInteger = 4;
-    const firstDecimal = 4.0;
-    const firstString = 'HackerRank ';
-
-    
-    console.log(firstInteger + parseInt(secondInteger));
-    console.log(firstDecimal + parseFloat(secondDecimal));
-    console.log(firstString + secondString);
+  console.log(firstInteger + parseInt(secondInteger));
+  console.log(firstDecimal + parseFloat(secondDecimal));
+  console.log(firstString + secondString);
 }
